141b520787da05949f415095d2053e3be24deb3f
Dmitrij <dmitry.bermas@gmail.com>
2025-04-06 21:39:34 +0200

    feat: created dist

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,17 @@ import vue from '@vitejs/plugin-vue'
 import path from "path"
 
 export default defineConfig({
+    base: '/',
     plugins: [ vue() ],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "src"),
             'assets': 'src/assets/',
           },
-    }
+    },
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true,
+        sourcemap: false,
+      },
 })
