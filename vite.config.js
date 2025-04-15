7cc98271b6c23e62a19dbf2f6fe3feeae3627978
Dmitrij <dmitry.bermas@gmail.com>
2025-04-15 13:44:55 +0200

    fix(vite.config.js): changed the base path for the deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from "path"
 
 export default defineConfig({
-    base: './',
+    base: '/bermas.dmytro.ui.quest.groupbwt.dev/',
     plugins: [ vue() ],
     resolve: {
         alias: {
